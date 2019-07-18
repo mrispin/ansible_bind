764dc3bea4cba85da255bfe5853e5b63b78bc493
Mark Rispin <mark@risp.in>
2019-07-18 23:55:59 +0100

    Install bind9 and dnsutils

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,3 +4,8 @@ import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_bind_installed(host):
+    bind = host.package("bind9")
+    assert bind.is_installed
