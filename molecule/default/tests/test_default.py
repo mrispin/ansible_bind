bdd7f6b7a095e4596ab85a06327edf369dcb725f
mark Rispin <mark@risp.in>
2019-07-19 11:45:58 +0000

    Start bind9 by default

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -9,3 +9,9 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_bind_installed(host):
     bind = host.package("bind9")
     assert bind.is_installed
+
+
+def test_bind_service_started(host):
+    bindservice = host.service("bind9")
+    assert bindservice.is_running
+    assert bindservice.is_enabled
