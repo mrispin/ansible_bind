873e8dd93da995c90e3b6dd7dc13af1c322d94cd
Mark Rispin <mark@risp.in>
2019-07-22 16:00:12 +0000

    Add tests for hosting a zone

--- molecule/primarymaster/tests/test_default.py
+++ molecule/primarymaster/tests/test_default.py
@@ -27,3 +27,18 @@ def test_bind_listening(host):
 def test_bind_forwarding_requests(host):
     google = host.addr("google.com")
     assert google.is_resolvable
+
+
+def test_bind_resolve_master(host):
+    servera = host.addr("servera.bind.test")
+    serverb = host.addr("serverb.bind.test")
+    mailservera = host.addr("mailservera.bind.test")
+    mailserverb = host.addr("mailserverb.bind.test")
+    assert servera.is_resolvable
+    assert '127.2.2.5' in servera.ipv4_addresses
+    assert serverb.is_resolvable
+    assert '127.4.2.5' in serverb.ipv4_addresses
+    assert mailservera.is_resolvable
+    assert '127.4.52.5' in mailservera.ipv4_addresses
+    assert mailserverb.is_resolvable
+    assert '127.4.62.5' in mailserverb.ipv4_addresses
